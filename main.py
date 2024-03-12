f7d2060599ddaee200d0814707a41b4f06a73be7
itsAndrewHunh <andrewhunhoff@gmail.com>
2024-03-12 10:46:27 -0500

    cleaned up for statement for key presses and created second 'hidden' circle

--- main.py
+++ main.py
@@ -13,20 +13,22 @@ __version__ = '1.0'
 __date__ = '2/18/2024'
 __status__ = 'Development'
 
-black = (0, 0, 0)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-white = (255, 255, 255)
-yellow = (255, 255, 0)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
 
-screen_size = screen_width, screen_height = 600, 400
-screen = pygame.display.set_mode(screen_size)
+SCREEN_SIZE = screen_width, screen_height = 600, 400
+SCREEN = pygame.display.set_mode(SCREEN_SIZE)
 
 # clock = pygame.time.Clock()
 fps_limit = 60
 
 global game
+
+
 
 
 def play_game():
@@ -40,7 +42,10 @@ def play_game():
     colorcircle = red
     posx = 300
     posy = 200
-    circle = pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
+
+    other_colorcircle = white
+    o_posx = 100
+    o_posy = 200
 
     while run_me:
         clock.tick(60)
@@ -52,25 +57,22 @@ def play_game():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    posx = posx - 10
+                    posx -= 10
 
-            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    posx = posx + 10
+                    posx += 10
 
-            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
-                    posy = posy + 10
+                    posy += 10
 
-            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    posy = posy - 10
+                    posy -= 10
 
         # fill the screen with black (otherwise, the circle will leave a trail)
         screen.fill(black)
 
-        # redraw the circle
-        pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
+        circle = pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
+        pygame.draw.circle(screen, other_colorcircle, (o_posx, o_posy), 50)
 
         pygame.display.flip()
 
